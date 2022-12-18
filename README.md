ea8d06617044218d474392f8999fa075f8880a6c
Reynaldo <reycastano97@gmail.com>
2022-12-17 18:45:56 -0600

    proyecto entregado

---
+++ README.md
@@ -0,0 +1,8 @@
+# Pairprogramming2
+
+Hola Milka,
+
+Añadi los links de idioma y centre los elementos.
+Mañana trabajare en el footer.
+
+Reynaldo.
---
+++ ejercicio.js
@@ -0,0 +1,36 @@
+//datos de entrada
+
+let pesoEnLibras = prompt(("Ingrese su peso en libras: "),10)
+
+// datos de salida
+
+let pesoEnKilos = pesoEnLibras * 0.453592
+
+console.log("Tu peso en kilogramos es de: "+pesoEnKilos)
+
+
+
+//Datos primitivos
+
+// String
+// Numero
+// Booleano
+// Nulo y undefined
+
+
+//Datos no primitivos
+
+//function 
+//object
+//Arreglos
+//ListaS
+
+//funciones
+
+function miFuncion(){
+    console.log('Hola Mundo')
+}
+
+function sumar( numero1, numero2){
+    console.log(numero1 + numero2)
+}
